d599dd725bc4e2468f3d34a5f1b76dcfbe382a99
Phaninder Pasupula <pasupulaphani@gmail.com>
2016-11-16 13:43:15 +0000

    use promised mocha

--- test/redis_cache.js
+++ test/redis_cache.js
@@ -1,4 +1,4 @@
-const should = require("should");
+require("should");
 const RedisCache = require("../lib/redis_cache");
 
 describe("redisCache", function () {
@@ -13,36 +13,19 @@ describe("redisCache", function () {
     const key = "captain-america";
     const value = "daddyIssues";
 
-    beforeEach(function (done) {
-      cache.deleteAll()
-        .then(function (v) {
-          v.should.be.ok();
-          done();
-        });
+    beforeEach(() => cache.deleteAll());
+
+    it("should set value without expiry if ttl is not provided", () => {
+
+      return cache.set(key, value)
+        .then(() => cache.get(key))
+        .should.eventually.be.equal(value);
     });
 
-    it("should set value without expiry if ttl is not provided", function (done) {
+    it("should set value with expiry if ttl is provided", () => {
 
-      cache.set(key, value)
-        .then(function (test) {
-          test.should.be.ok();
-        })
-        .then(function () {
-          return cache.get(key);
-        })
-        .then(function (v) {
-          v.should.be.equal(value);
-          done();
-        });
-    });
-
-    it("should set value with expiry if ttl is provided", function (done) {
-
-      cache.set("key", "value", 1)
-        .then(function (test) {
-          test.should.be.ok();
-          done();
-        });
+      return cache.set("key", "value", 1)
+        .should.eventually.be.ok();
     });
   });
 
@@ -51,29 +34,18 @@ describe("redisCache", function () {
     const key = "chuck-norris";
     const value = "superman";
 
-    before(function (done) {
-      cache.set(key, value)
-        .then(function () {
-          done();
-        });
+    before(() => cache.set(key, value));
+
+    it("should get the existing key", () => {
+
+      return cache.get(key)
+        .should.eventually.be.equal(value);
     });
 
-    it("should get the existing key", function (done) {
+    it("should not get the non-existing key", () => {
 
-      cache.get(key)
-        .then(function (v) {
-          v.should.be.equal(value);
-          done();
-        });
-    });
-
-    it("should not get the non-existing key", function (done) {
-
-      cache.get("nonExistingKey")
-        .then(function (v) {
-          should(v).not.be.ok();
-          done();
-        });
+      return cache.get("nonExistingKey")
+        .should.eventually.not.be.ok();
     });
   });
 
@@ -82,44 +54,28 @@ describe("redisCache", function () {
     const key = "chuck-norris";
     const value = "superman";
 
-    before(function (done) {
-      cache.deleteAll()
-        .then(() => done());
-    });
+    before(() => cache.deleteAll());
 
-    it("should set if key not exists", function (done) {
+    it("should set if key not exists", () => {
 
-      cache.wrap(key, value)
-        .then(function (v) {
-          v.should.be.equal(value);
-        })
+      return cache.wrap(key, value)
+        .then(v => v.should.be.equal(value))
         .then(() => cache.get(key))
-        .then(function (v) {
-          v.should.be.equal(value);
-          done();
-        });
+        .should.eventually.be.equal(value);
     });
 
-    it("should get if key exists", function (done) {
+    it("should get if key exists", () => {
 
-      cache.set(key, value)
-        .then(function (v) {
-          v.should.be.ok();
-        })
+      return cache.set(key, value)
+        .then(v => v.should.be.ok())
         .then(() => cache.wrap(key))
-        .then(function (v) {
-          v.should.be.equal(value);
-          done();
-        });
+        .should.eventually.be.equal(value);
     });
 
-    it("should do nothing when ttlInSeconds=0", function (done) {
+    it("should do nothing when ttlInSeconds=0", () => {
 
-      cache.wrap(key, value, 0)
-        .then(function (v) {
-          v.should.be.equal(value);
-          done();
-        });
+      return cache.wrap(key, value, 0)
+        .should.eventually.be.equal(value);
     });
   });
 
@@ -127,58 +83,46 @@ describe("redisCache", function () {
 
     const keyValues = {key1: "value1", key2: "value2"};
 
-    before(function (done) {
-      cache.deleteAll()
-        .then(() => done());
+    before(() => cache.deleteAll());
+
+    beforeEach(() => Promise.all(Object.keys(keyValues)
+        .map(key => cache.set(key, keyValues[key]))));
+
+    it("should return all the keys", () => {
+
+      return cache.keys()
+        .then(keys => keys.map(k => Object.keys(keyValues).should.containEql(k)));
     });
 
-    beforeEach(function (done) {
-      Promise.all(Object.keys(keyValues)
-        .map(key => cache.set(key, keyValues[key]))
-      ).then(() => done());
-    });
+    it("should return all the keys matches pattern", () => {
 
-    it("should return all the keys", function (done) {
-
-      cache.keys()
-        .then(keys => {
-          keyValues.should.have.keys(keys[0], keys[1]);
-          done();
-        });
-    });
-
-    it("should return all the keys matches pattern", function (done) {
-
-      cache.keys("key[2]")
-        .then(keys => {
-          keys.should.containEql("key2");
-          done();
-        });
+      return cache.keys("key[2]")
+        .should.eventually.containEql("key2");
     });
   });
 
   describe("deleteAll", function () {
 
-    beforeEach(function (done) {
-      const keyValues = {key1: "value1", key2: "value2"};
+    const keyValues = {key1: "value1", key2: "value2"};
+    beforeEach(() => Promise.all(Object.keys(keyValues)
+        .map(key => cache.set(key, keyValues[key]))));
 
-      Promise.all(Object.keys(keyValues)
-        .map(key => cache.set(key, keyValues[key]))
-      ).then(() => done());
-    });
+    it("should delete all the keys", () => {
 
-    it("should delete all the keys", function (done) {
-
-      cache.deleteAll()
-        .then(function (v) {
-          v.should.be.ok();
-        })
+      return cache.deleteAll()
+        .then(v => v.should.be.ok())
         .then(() => cache.keys())
-        .then(keys => {
-          keys.should.be.empty();
-          done();
-        })
-        .catch(e => console.log(e));
+        .should.eventually.be.empty();
     });
+  });
+
+  describe("Connection fail", function () {
+
+    it("should throw error on initialization", () => {
+
+    });
+  });
+
+  describe("Store not available", function () {
   });
 });
