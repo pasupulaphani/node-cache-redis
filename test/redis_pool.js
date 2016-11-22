1a522f27840f6a4024f4a3fde50305e7c0707f71
Phaninder Pasupula <pasupulaphani@gmail.com>
2016-11-22 18:26:44 +0000

    added tests

--- test/redis_pool.js
+++ test/redis_pool.js
@@ -3,27 +3,150 @@ const RedisPool = require("../lib/redis_pool");
 
 describe("redisPool", () => {
 
+  const redisOptions = {
+    host: process.env.REDIS_HOST || "127.0.0.1"
+  };
+
+  describe("constructor", () => {
+
+    it("should set db when sent as constructor option", () => {
+
+      redisOptions.db = 1;
+
+      const pool = new RedisPool("testPool", redisOptions);
+
+      return pool.acquire(0)
+        .then(c => c.selected_db)
+        .should.eventually.be.equal(redisOptions.db);
+    });
+  });
+
   describe("acquire", () => {
 
     it("should acquire connection with valid host", () => {
-      const redisOptions = Object.assign({
-        host: process.env.REDIS_HOST || "127.0.0.1"
-      });
+
       const pool = new RedisPool("testPool", redisOptions);
 
       return pool.acquire()
-        .should.be.ok();
+        .should.eventually.be.ok();
+    });
+
+    it("should acquire connection to db when set", () => {
+
+      const pool = new RedisPool("testPool", redisOptions);
+      const db = 1;
+
+      return pool.acquire(0, db)
+        .then(c => c.selected_db)
+        .should.eventually.be.equal(db);
     });
 
     it("should not acquire connection with invalid host", () => {
-      const redisOptions = Object.assign({
-        host: "UNAVAILABLE_HOST"
-      });
+      redisOptions.host = "UNAVAILABLE_HOST";
 
       const pool = new RedisPool("testCache", redisOptions);
 
       // this is due to the limitation of node-pool ATM
       return pool.acquire().should.eventually.be.instanceOf(Error);
+    });
+  });
+
+  describe("release", () => {
+
+    const poolOptions = {
+      min: 2,
+      max: 4
+    };
+    const pool = new RedisPool("testPool", redisOptions, poolOptions);
+
+    it("should release connection with valid host", () => {
+
+      pool.availableObjectsCount().should.be.equal(poolOptions.min);
+      pool.getPoolSize().should.be.equal(poolOptions.min);
+
+      return pool.acquire()
+        .then(client => {
+          pool.availableObjectsCount().should.be.equal(poolOptions.min - 1);
+          return pool.release(client);
+        })
+        .then(() => pool.availableObjectsCount().should.be.equal(poolOptions.min));
+    });
+  });
+
+  describe("drain", () => {
+
+    const poolOptions = {
+      min: 2,
+      max: 4
+    };
+    const pool = new RedisPool("testPool", redisOptions, poolOptions);
+
+    it("should drain all the coonections", () => {
+
+      pool.availableObjectsCount().should.be.equal(poolOptions.min);
+      pool.getPoolSize().should.be.equal(poolOptions.min);
+
+      return pool.drain()
+        .then(() => {
+          pool.availableObjectsCount().should.be.equal(poolOptions.min);
+          pool.getPoolSize().should.be.equal(poolOptions.min);
+        });
+      });
+  });
+
+  describe("getName", () => {
+
+    it("should set given name", () => {
+
+      const name = "testPool";
+      const pool = new RedisPool(name, redisOptions);
+
+      pool.getName().should.be.equal(name);
+    });
+  });
+
+  describe("getRedisOptions", () => {
+
+    it("should set given redis options", () => {
+
+      const name = "testPool";
+      const pool = new RedisPool(name, redisOptions);
+
+      pool.getRedisOptions().should.be.equal(redisOptions);
+    });
+  });
+
+  describe("getPoolOptions", () => {
+
+    it("should set given pool options", () => {
+
+      const name = "testPool";
+      const poolOptions = {
+        min: 2,
+        max: 4
+      };
+      const pool = new RedisPool(name, redisOptions, poolOptions);
+
+      pool.getPoolOptions().should.be.equal(poolOptions);
+    });
+  });
+
+  describe("status", () => {
+
+    it("should set given pool options", () => {
+
+      const name = "testPool";
+      const poolOptions = {
+        min: 2,
+        max: 4
+      };
+      const pool = new RedisPool(name, redisOptions, poolOptions);
+
+      const status = pool.status();
+      status.name.should.be.equal(name);
+      status.size.should.be.equal(poolOptions.min);
+      // status.avail.should.be.equal(poolOptions.min); // todo debug
+      status.waiting.should.be.equal(0);
     });
   });
 });
