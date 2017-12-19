2c5e07761688a9078f7d08b50533d82afacc67aa
phani <pasupulaphani@gmail.com>
2017-12-19 16:13:41 +0000

    removed unused scripts

--- scripts/gh_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z "docs" ]
-then
-  echo "No docs folder found to deploy to ghpages"
-  exit 1
-fi
-git add docs && git commit -m 'docs cmit prepublish'
-git push origin `git subtree split --prefix docs master`:gh-pages --force
